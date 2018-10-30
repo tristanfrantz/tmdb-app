00b4e4410439bdb14f6fb70312f91e70ed3254c6
trilli147 <tristanjfz@gmail.com>
2018-10-30 23:03:29 +0000

    minor

--- components/MovieDetails.js
+++ components/MovieDetails.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   contentContainer: {
-    padding: 15,
+    padding: 8,
   },
   movieContainer: {
     flexDirection: 'row',
