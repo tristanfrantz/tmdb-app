8dd6c9682c93df0134dde3ea34890b24dd999faf
trilli147 <tristanjfz@gmail.com>
2018-10-31 03:35:24 +0000

    fixed panels for ios

--- components/DetailsPanel.js
+++ components/DetailsPanel.js
@@ -1,21 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
-import Panel from 'react-native-panel';
+import {
+  StyleSheet, View, TouchableOpacity, Text,
+} from 'react-native';
+import Collapsible from 'react-native-collapsible';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'lightgrey',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  toggleContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  toggleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  iconContainer: {
+    paddingRight: 8,
+  },
+  contentContainer: {
+    flex: 1,
+    paddingBottom: 10,
+  },
+});
 
 class DetailsPanel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isCollapsed: true,
+    };
   }
+
+  toggle = () => {
+    const { isCollapsed } = this.state;
+    this.setState({ isCollapsed: !isCollapsed });
+  };
 
   render() {
     const { title, content } = this.props;
+    const { isCollapsed } = this.state;
     return (
-      <Panel header={title}>
-        <Text style={styles.myDescription}>{content}</Text>
-      </Panel>
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.toggleContainer} onPress={() => this.toggle()}>
+          <View>
+            <Text style={styles.toggleText}>{title}</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            {isCollapsed ? (
+              <Icon size={22} name="angle-down" style={styles.addIcon} />
+            ) : (
+              <Icon size={22} name="angle-up" style={styles.addIcon} />
+            )}
+          </View>
+        </TouchableOpacity>
+        <Collapsible collapsed={isCollapsed}>
+          <View style={styles.contentContainer}>
+            <Text style={styles.content}>{content}</Text>
+          </View>
+        </Collapsible>
+      </View>
     );
   }
 }
