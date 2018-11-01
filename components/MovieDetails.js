89fde24121701807f3bfa767094f13947688ff88
Hannes Jóhannsson <hannes16@ru.is>
2018-11-01 00:16:57 +0000

    loading while fetching

--- components/MovieDetails.js
+++ components/MovieDetails.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import {
-  StyleSheet, TouchableOpacity, ScrollView, View, Text, Image,
+  StyleSheet,
+  TouchableOpacity,
+  ScrollView,
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { connect } from 'react-redux';
@@ -67,65 +73,73 @@ class MovieDetails extends React.Component {
     super(props);
     this.state = {
       item: {},
+      loading: false,
     };
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     const { imdbID } = this.props.navigation.state.params;
     const movie = this.props.movies.filter(m => m.key === imdbID)[0];
-
     if (movie) {
       this.setState({ item: movie.details });
     } else {
+      this.setState({ loading: true });
       const apiKey = '14cfd31';
-
-      fetch(`http://omdbapi.com/?apikey=${apiKey}&i=${imdbID}`)
+      await fetch(`http://omdbapi.com/?apikey=${apiKey}&i=${imdbID}`)
         .then(res => res.json())
         .then(res => this.setState({ item: res }))
         .catch(err => console.log(err));
+      this.setState({ loading: false });
     }
   }
 
   render() {
     const movie = this.state.item;
+    const { loading } = this.state;
 
     return (
       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-        <View style={styles.movieContainer}>
-          <Image style={styles.poster} source={{ uri: movie.Poster }} />
-          <View style={styles.titleContainer}>
-            <Text style={styles.titleText}>
-              {movie.Title}
-              <Text style={styles.year}>{` (${movie.Year})`}</Text>
-            </Text>
-            <View style={styles.detailsContainer}>
-              <ImdbRating rating={movie.imdbRating} votes={movie.imdbVotes} />
-              <Text style={styles.text}>
-                {'Genre: '}
-                <Text style={styles.shadowText}>{movie.Genre}</Text>
-              </Text>
-              <Text style={styles.shadowText}>{`Rated ${movie.Rated} | ${movie.Runtime}`}</Text>
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <View>
+            <View style={styles.movieContainer}>
+              <Image style={styles.poster} source={{ uri: movie.Poster }} />
+              <View style={styles.titleContainer}>
+                <Text style={styles.titleText}>
+                  {movie.Title}
+                  <Text style={styles.year}>{` (${movie.Year})`}</Text>
+                </Text>
+                <View style={styles.detailsContainer}>
+                  <ImdbRating rating={movie.imdbRating} votes={movie.imdbVotes} />
+                  <Text style={styles.text}>
+                    {'Genre: '}
+                    <Text style={styles.shadowText}>{movie.Genre}</Text>
+                  </Text>
+                  <Text style={styles.shadowText}>{`Rated ${movie.Rated} | ${movie.Runtime}`}</Text>
+                </View>
+              </View>
             </View>
-          </View>
-        </View>
-        <AddWishlistButton movie={movie} />
+            <AddWishlistButton movie={movie} />
 
-        <TouchableOpacity
-          style={styles.plotContainer}
-          onPress={() => this.props.navigation.navigate('Plot', movie)}
-        >
-          <View style={styles.plotTextContainer}>
-            <Text>{movie.Plot}</Text>
-          </View>
-          <View style={styles.plotArrow}>
-            <Icon size={22} name="angle-right" />
-          </View>
-        </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.plotContainer}
+              onPress={() => this.props.navigation.navigate('Plot', movie)}
+            >
+              <View style={styles.plotTextContainer}>
+                <Text>{movie.Plot}</Text>
+              </View>
+              <View style={styles.plotArrow}>
+                <Icon size={22} name="angle-right" />
+              </View>
+            </TouchableOpacity>
 
-        <DetailsPanel title="Actors" content={movie.Actors} />
-        <DetailsPanel title="Directors" content={movie.Director} />
-        <DetailsPanel title="Writers" content={movie.Writer} />
-        <DetailsPanel title="Awards" content={movie.Awards} />
+            <DetailsPanel title="Actors" content={movie.Actors} />
+            <DetailsPanel title="Directors" content={movie.Director} />
+            <DetailsPanel title="Writers" content={movie.Writer} />
+            <DetailsPanel title="Awards" content={movie.Awards} />
+          </View>
+        )}
       </ScrollView>
     );
   }
