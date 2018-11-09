b7d7cd7ed64890d5bb04363442ba6f5c4db44360
ivarmarkusson <ivarmar15@ru.is>
2018-11-09 20:58:37 +0000

    season list

--- components/Seasons.js
+++ components/Seasons.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList, Image } from 'react-native';
 import { Bubbles } from 'react-native-loader';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  listItemContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 5,
+    margin: 5,
+  },
+  poster: {
+    height: 100,
+    width: 100,
+    marginRight: 5,
+  },
+  textContainer: {
+    flexDirection: 'column',
+  },
+  textTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
@@ -30,7 +48,22 @@ class Seasons extends React.Component {
     }
   }
 
-  renderItem = ({ item }) => <Text>{item.name}</Text>;
+  renderItem = ({ item }) => {
+    return (
+      <View style={styles.listItemContainer}>
+        <Image
+          style={styles.poster}
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}`,
+          }}
+        />
+        <View style={styles.textContainer}>
+          <Text style={styles.textTitle}>{item.name}</Text>
+          <Text>episodes: {item.episode_count}</Text>
+        </View>
+      </View>
+    );
+  };
 
   render() {
     const { loading, error, seasons } = this.state;
